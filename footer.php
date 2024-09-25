437e2ba0477d6136a9ae1c234e2dc252d5c548ce
NHarika01 <168948144+NHarika01@users.noreply.github.com>
2024-09-25 18:47:57 +0530

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,10 +1,28 @@
-<!-- Footer Section Starts -->
-<div class="footer">
-            <div class="wrapper">
-                <p class="text-center">GCTC All rights reserved, Developed By - <a href="#"> CanteenMate Team</a></p>
-            </div>
+<!-- social Section Starts Here -->
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="https://wa.me/917780438599"><img src="https://img.icons8.com/?size=55&id=16713&format=png&color=000000"/></a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/_harikaa_06"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+            </ul>
         </div>
-        <!-- Footer Section Ends -->
+    </section>
+    <!-- social Section Ends Here -->
 
-    </body>
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="#">CanteenMate Team</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
 </html>
