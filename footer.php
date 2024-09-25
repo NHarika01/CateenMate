fd88ba5f40a936f729e306a0a410d13592223ab2
NHarika01 <168948144+NHarika01@users.noreply.github.com>
2024-09-25 18:46:14 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">GCTC All rights reserved, Developed By - <a href="#"> CanteenMate Team</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
